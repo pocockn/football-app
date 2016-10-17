1e095f9faeb94978e6b9aafe09f0343648119871
Nick Pocock <pocockn@hotmail.co.uk>
2016-10-17 18:06:11 +0100

    add goals to player model, display goals in template

--- app/models/player.js
+++ app/models/player.js
@@ -3,12 +3,14 @@ import Faker from 'faker';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
+  goals: DS.attr('number'),
   team: DS.belongsTo('team', {inverse: 'players', async: true}),
 
   randomize(team) {
     this.set('name', Faker.company.companyName());
+    this.set('goals', Faker.random.number());
     this.set('team', team);
     return this;
   }
-  
+
 });
